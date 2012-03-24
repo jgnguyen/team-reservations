9261225c0aa28e03488604631aaaadf7f988f3d6
unknown <Andrew@Caerulius.(none)>
2012-03-24 14:50:17 -0400

    added flashlight functionality

--- gen/mobile/homework/R.java
+++ gen/mobile/homework/R.java
@@ -18,6 +18,10 @@ public final class R {
         public static final int ic_launcher=0x7f020004;
         public static final int panotify=0x7f020005;
     }
+    public static final class id {
+        public static final int buttonOff=0x7f050001;
+        public static final int buttonOn=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
